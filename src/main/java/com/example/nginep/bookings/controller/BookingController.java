f952e389374cf7fb0a58c6ebf25193c9e2e1622e
irfazbow <irfanefp@gmail.com>
2024-08-28 22:25:19 +0700

    refactor: add the standard response for booking controller

--- src/main/java/com/example/nginep/bookings/controller/BookingController.java
+++ src/main/java/com/example/nginep/bookings/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.example.nginep.bookings.controller;
 import com.example.nginep.bookings.dto.CreateBookingDTO;
 import com.example.nginep.bookings.entity.Booking;
 import com.example.nginep.bookings.service.BookingService;
+import com.example.nginep.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,8 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping("/create")
-    public ResponseEntity<Booking> createBooking(@RequestBody CreateBookingDTO bookingDTO) {
+    public ResponseEntity<Response<Booking>> createBooking(@RequestBody CreateBookingDTO bookingDTO) {
         Booking newBooking = bookingService.createBooking(bookingDTO);
-        return ResponseEntity.ok(newBooking);
+        return Response.successResponse("Booking created successfully", newBooking);
     }
 }
